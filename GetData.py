ef86393ef6794a136dc5327b2c080c0e6ebbc7a8
Merthew-Deathson <merthewd@gmail.com>
2018-09-23 12:08:31 -0400

    Bug fix

--- GetData.py
+++ GetData.py
@@ -11,28 +11,7 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import *
 from matplotlib.figure import Figure
 
-# Ramhacks 2018 Altria Project: Sentinment Analysis of a Brand or Name on Social Media(Reddit)
-# Uses Praw API to scrape a subreddit's posts and comments based on the args passed in from
-# the GUI, using this data the program calculates sentinment using Natural Language Toolkit's
-# bulit in API Vader which uses pre trained data to determine the polarity of a comment or 
-# post content(if it's a text post)
-#---------------------------------------
-#	@Authors Byron Stearns, Timothy Patterson
-
-
-
-# main class used to pull in data from reddit and use 2 temporary text files to sort and run vader API
 class mclass:
-
-
-
-
-# Intializes the GUI for the program to display the graph, and 
-# to take in data from entry textfields
-# @Param self - sets the root gui directory
-# @Param root - root directory for gui
-
-
 	def __init__(self, root):
 		self.root = root
 		
@@ -40,7 +19,7 @@ class mclass:
 		
 		
 		self.l1=Label(root,text="Subreddits to Search")
-		self.l2=Label(root,text="Keyword to Search")   #Titles for gui elements
+		self.l2=Label(root,text="Keyword to Search")
 		self.l3=Label(root,text="Max Number of Entries")
 		self.e1 = Entry(root, width = 40)
 		self.e2 = Entry(root, width = 40)
@@ -61,20 +40,13 @@ class mclass:
 		
 		b=Button(root, text="Run", width=20, command = self.run)
 		b.pack()
-
-
-# Creates a graph based off the data found from the scraper
-# and attaches it to the gui
-# @Param self - gui
-
-
 		
 	def plot (self):
 		
 		file = open("data.txt", "r")
 		holder = file.read()
 		
-		holder = [x for x in  re.split(' ', holder) if x] 
+		holder = [x for x in  re.split(' ', holder) if x]
 		
 		dates = []
 		values = []
@@ -83,79 +55,79 @@ class mclass:
 
 		for x in range(int(len(holder)/3)):
 			dates.append(holder[(3*x)])
-		dates=[re.compile(r"-").sub("", m) for m in dates]   #pulls in dates from data file and formats accordingly
+		dates=[re.compile(r"-").sub("", m) for m in dates]
 		for x in range(int(len(holder)/3)):
 			values.append(float(holder[(3*x)+2]))
-	
 		
+		sorted_values = [x for _,x in sorted(zip(dates,values))]
 		dates.sort()
-		values.sort()
-		x = [datetime.strptime(d, '%Y%m%d').strftime('%m/%d/%Y') for d in dates]   #sets x axis to the dates 
 		
-		fig = Figure(figsize=(6,6))
+		
+		#dates.sort()
+		#values.sort()
+		x = [datetime.strptime(d, '%Y%m%d').strftime('%m/%d/%Y') for d in dates]
+		#y = [data_values[]]
+		
+		fig = Figure(figsize=(10,6))
 		a = fig.add_subplot(111)
-		a.scatter(x,values)
-		a.set_title ("Estimation Grid", fontsize=16)
+		a.scatter(x,sorted_values)
+		a.set_title ("Sentiment vrs Time", fontsize=16)
 		a.set_ylabel("Sentiment", fontsize=14)
 		a.set_xlabel("Date", fontsize=14)
 		
-		a.set_xticks(x[::25]) 
+		#a.set_xticks(x[::20]) 
 		
 		canvas = FigureCanvasTkAgg(fig, master=self.root)
 		canvas.get_tk_widget().pack()
 		canvas.draw()
 		
-# Creates a new textfile and writes to the textfile the info obtained from the scraper
-# @Param self - data from the textfields
-
-
 
 	def run(self):
 		file_input = open('input.txt', 'w')
 
 		file_input.write(self.e1.get())
 		file_input.write("\n")
-		file_input.write(self.e2.get())    #Write to file the values pulled in from GUI
+		file_input.write(self.e2.get())
 		file_input.write("\n")
 		file_input.write(self.e3.get())
 		file_input.close()
 		
 		
-		reddit = praw.Reddit(client_id='CLIENT_ID',        #OAuth Call  
-						client_secret="CLIENT_SECRET",
-						user_agent='USER_AGENT')
+		reddit = praw.Reddit(client_id='SU3DL2_kxAdtqw',
+						client_secret="hq0dRanTH4gmFcN4tfbFXcAcKeA",
+						user_agent='RamhacksMW')
 					 
 		file_input = open('input.txt', 'r')
 	
-		sin = file_input.read()   #Read from file and sort
+		sin = file_input.read()
 		sf = sin.split('\n')
 		sub_name = sf[0]
 		sub_search = sf[1]
 		limit_num = int(float(sf[2]))
 
-		subreddit = reddit.subreddit(sub_name)   # subreddit instances
+		subreddit = reddit.subreddit(sub_name)
 
 		data_subreddit = subreddit.search(sub_search,limit=limit_num)
 
-		file_data = open("data.txt", 'w')  #New file to write data obtained from PRAW api and args passed from gui
+		file_data = open("data.txt", 'w')
 
-		for submission in data_subreddit:  #for loop that runs through each submission in subreddit instance
+		for submission in data_subreddit:
 	
 			def get_date(created):
 				return dt.datetime.fromtimestamp(created)
 	
 	
-			dataT=submission.title  #Submission Title
-			dataB=submission.selftext  #Submission Body Text
-			dataC=submission.comments.list()  #Submission Comments
-			dataD=submission.created #Submission Timestamp when comment is create 
-			dataTD=get_date(dataD)  #Gets the data from the created instance
+			dataT=submission.title
+			dataB=submission.selftext
+			dataC=submission.comments.list()
+			dataD=submission.created
+			dataTD=get_date(dataD)
 	
 			file_data.write(" ")
-			file_data.write(str(dataTD))  #writes data found to disk
+			file_data.write(str(dataTD))
 			file_data.write(" ")
 	
-			sid = SentimentIntensityAnalyzer()   #Vader API call to determine the sentimental value of a comment or body text
+			sid = SentimentIntensityAnalyzer()
 			infull = ""
 			infullc = ""
 		
@@ -165,14 +137,14 @@ class mclass:
 				infull += dataB
 	
 			submission.comments.replace_more()
-			l = submission.comments.list()    #Grabs all replies 
+			l = submission.comments.list()
 			for x in range(len(l)):
 				infullc += l[x].body
 				infullc += " "
 	
-			infullc = infullc.lower() # makes sure each submission is lower case to ensure acurate comparasion
+			infullc = infullc.lower()
 	
-			setToken = sent_tokenize(infull)   #tokenizes comments
+			setToken = sent_tokenize(infull)
 			setTokenc = sent_tokenize(infullc)
 	
 			ogpost_score = 0
@@ -181,7 +153,7 @@ class mclass:
 	
 			for sen in setToken:
 				ss=sid.polarity_scores(sen)
-				s=ss['compound']     # Calculates sentiment values using sid/vader api
+				s=ss['compound']
 		
 				ogpost_score += s
 				total_score += s
@@ -189,7 +161,7 @@ class mclass:
 		
 			print(ogpost_score)
 			for sen in setTokenc:
-				ss=sid.polarity_scores(sen)      #Calculates sentinment for body text and if the body text is negative negate all comment values
+				ss=sid.polarity_scores(sen)
 				s=ss['compound']
 				if ogpost_score < 0:
 					s = -s
@@ -198,11 +170,11 @@ class mclass:
 				total_score += s
 				counter += 1
 	
-			average_score = total_score / counter  # take average
-			file_data.write(str(average_score))   # write the average to a text file
+			average_score = total_score / counter
+			file_data.write(str(average_score))
 
 		file_data.close()
-		self.plot()   #plot method call
+		self.plot()
 	
 	
 	
